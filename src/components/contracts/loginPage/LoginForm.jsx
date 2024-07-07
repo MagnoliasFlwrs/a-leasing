5c73ddfb1956d864542c2cd8d4ab9b84a0087ef8
Savenok Marta <magnolia.sav@gmail.com>
2024-07-08 00:29:35 +0300

    create registration block

--- src/components/contracts/loginPage/LoginForm.jsx
+++ src/components/contracts/loginPage/LoginForm.jsx
@@ -4,6 +4,8 @@ import {Checkbox, PinInput, PinInputField} from "@chakra-ui/react";
 const LoginForm = () => {
     const [login, setLogin] = useState('');
     const [loginErr, setLoginErr] = useState(false);
+    const [newLogin, setNewLogin] = useState('');
+    const [newLoginErr, setNewLoginErr] = useState(false);
     const [password, setPassword] = useState('');
     const [passwordErr, setPasswordErr] = useState('');
     const [isValidLogin, setIsValidLogin] = useState(false);
@@ -12,6 +14,21 @@ const LoginForm = () => {
 
     useEffect(() => {
         const input = document.querySelector('.login-input');
+        if (input) {
+            input.addEventListener("input", mask, false);
+            input.addEventListener("focus", mask, false);
+            input.addEventListener("blur", mask, false);
+        }
+        return () => {
+            if (input) {
+                input.removeEventListener("input", mask, false);
+                input.removeEventListener("focus", mask, false);
+                input.removeEventListener("blur", mask, false);
+            }
+        };
+    }, []);
+    useEffect(() => {
+        const input = document.querySelector('.new-input');
         if (input) {
             input.addEventListener("input", mask, false);
             input.addEventListener("focus", mask, false);
@@ -62,6 +79,11 @@ const LoginForm = () => {
         setLogin(value);
         console.log(value)
     }
+    const handleNewLogin = (e) => {
+        const { value } = e.target;
+        setNewLogin(value);
+        console.log(newLogin)
+    }
 
     const handlePass = (e) => {
         const { value } = e.target;
@@ -87,6 +109,14 @@ const LoginForm = () => {
         setSelectedAccount(account);
         setIsChoosenAccount(true);
     };
+
+    const hideRegistrationBlock = () => {
+        document.querySelector('.registration-block').classList.remove('open');
+    }
+    const openRegistrationBlock = (e) => {
+        e.preventDefault()
+        document.querySelector('.registration-block').classList.add('open');
+    }
 
     return (
         <div className='login-form'>
@@ -152,7 +182,7 @@ const LoginForm = () => {
                     </a>
                     <p>
                         Нет аккаунта?
-                        <a href="#">Зарегестрироваться</a>
+                        <a href="#" onClick={(e)=> openRegistrationBlock(e)}>Зарегестрироваться</a>
                     </p>
                 </div>
             </div>
@@ -233,6 +263,48 @@ const LoginForm = () => {
                     <p className='no-account'>
                         Нет аккаунта?
                         <a href="#">Зарегестрироваться</a>
+                    </p>
+                </div>
+            </div>
+            <div className="registration-block ">
+                <div className="back-btn" onClick={()=>  hideRegistrationBlock()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none">
+                        <path fillRule="evenodd" clipRule="evenodd"
+                              d="M2.66675 8.49967C2.66675 8.13148 2.96522 7.83301 3.33341 7.83301H12.6667C13.0349 7.83301 13.3334 8.13148 13.3334 8.49967C13.3334 8.86786 13.0349 9.16634 12.6667 9.16634H3.33341C2.96522 9.16634 2.66675 8.86786 2.66675 8.49967Z"
+                              fill="#0070C9"/>
+                        <path fillRule="evenodd" clipRule="evenodd"
+                              d="M8.47149 3.36225C8.73184 3.6226 8.73184 4.04471 8.47149 4.30506L4.27622 8.50033L8.47149 12.6956C8.73184 12.9559 8.73184 13.378 8.47149 13.6384C8.21114 13.8987 7.78903 13.8987 7.52868 13.6384L2.86201 8.97173C2.60166 8.71138 2.60166 8.28927 2.86201 8.02892L7.52868 3.36225C7.78903 3.1019 8.21114 3.1019 8.47149 3.36225Z"
+                              fill="#0070C9"/>
+                    </svg>
+                    <span>Вернуться назад</span>
+                </div>
+                <div className="registration-container">
+                    <p className="title">Регистрация</p>
+                    <div className="row">
+                        <div className="input-box">
+                            <input
+                                type="text"
+                                className={loginErr ? 'new-input error' : 'new-input'}
+                                onChange={(e) => handleLogin(e)}
+                                value={login}
+                            />
+                            {
+                                loginErr && <span>Неверный формат номера телефона</span>
+                            }
+
+                        </div>
+                        <button onClick={()=> validateLogin()}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="37" height="36" viewBox="0 0 37 36"
+                                 fill="none">
+                                <path fillRule="evenodd" clipRule="evenodd"
+                                      d="M12.9393 7.93934C13.5251 7.35355 14.4749 7.35355 15.0607 7.93934L24.0607 16.9393C24.6464 17.5251 24.6464 18.4749 24.0607 19.0607L15.0607 28.0607C14.4749 28.6464 13.5251 28.6464 12.9393 28.0607C12.3536 27.4749 12.3536 26.5251 12.9393 25.9393L20.8787 18L12.9393 10.0607C12.3536 9.47487 12.3536 8.52513 12.9393 7.93934Z"
+                                      fill="white"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <p className='acc'>
+                        Уже есть аккаунт?
+                        <a href="#" onClick={()=>  hideRegistrationBlock()}>Войти</a>
                     </p>
                 </div>
             </div>

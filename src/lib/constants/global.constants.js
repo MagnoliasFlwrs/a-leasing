3b3f4b1d259bc73ac97b6e1a76ac5d546fdc7cb7
Rovshen Atayev <at.rovshan@gmail.com>
2025-03-20 14:34:58 +0500

    Fix url

--- src/lib/constants/global.constants.js
+++ src/lib/constants/global.constants.js
@@ -1,2 +1,2 @@
 // export const API_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_URL = 'http://client.a-leasing.by/api/v1/';
+export const API_URL = 'https://client.a-leasing.by/api/v1/';
